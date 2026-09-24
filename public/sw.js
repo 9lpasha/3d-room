3d299d969f3c0fb4585e56965061d2ba1803754c
Churakov Pavel <131313pasha@gmail.com>
2026-09-24 10:42:19 +0300

    fix icons

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "dev-path-v3";
+const CACHE_NAME = "dev-path-v4";
 const scopeUrl = new URL(self.registration.scope);
 const appShell = [new URL("./", scopeUrl).href, new URL("./index.html", scopeUrl).href];
 
